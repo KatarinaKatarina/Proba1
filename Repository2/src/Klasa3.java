53a0ff1f61bdbcc89475de9667f8384fcd817734
NebojsaIvanic <ivanicn86@gmail.com>
2017-11-06 13:51:01 +0100

    promena po nebojsi

--- Repository2/src/Klasa3.java
+++ Repository2/src/Klasa3.java
@@ -3,7 +3,8 @@ public class Klasa3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("juhuuu i ja sam tu.");
+System.out.println("asdf");
 	}
 
 }
